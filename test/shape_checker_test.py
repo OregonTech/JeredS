79bee446338aa20f73f6ef7c40f0d28930448f97
JeredSundquist <jeredjsundquist@hotmail.com>
2016-01-15 12:37:06 -0800

    Lab1 revised please check email

--- test/shape_checker_test.py
+++ test/shape_checker_test.py
@@ -6,167 +6,167 @@ from source.shape_checker import get_square_type
 from unittest import TestCase
 import math
 
-# class TestGetTriangleType(TestCase):
-#     "EQUILATERAL with all INT"
-#
-#     def test_get_triangle_equilateral_all_int(self):
-#         result = get_triangle_type(1, 1, 1)
-#         self.assertEqual(result, 'equilateral')
-#
-#     "EQUILATERAL with mixed type"
-#
-#     def test_get_triangle_equilateral_all_mixedTypeOne(self):
-#         result = get_triangle_type(1.0, 1, 1)
-#         self.assertEqual(result, 'equilateral')
-#
-#     def test_get_triangle_equilateral_all_mixedTypeTwo(self):
-#         result = get_triangle_type(1, 1.0, 1)
-#         self.assertEqual(result, 'equilateral')
-#
-#     def test_get_triangle_equilateral_all_mixedTypeThree(self):
-#         result = get_triangle_type(1, 1, 1.0)
-#         self.assertEqual(result, 'equilateral')
-#
-#     "SCALENE with all INT"
-#
-#     def test_get_triangle_scalene_all_int(self):
-#         result = get_triangle_type(4, 3, 2)
-#         self.assertEqual(result, 'scalene')
-#
-#     "SCALENE with mixed type"
-#
-#     def test_get_triangle_scalene_all_mixedTypeOne(self):
-#         result = get_triangle_type(4.0, 3, 2)
-#         self.assertEqual(result, 'scalene')
-#
-#     def test_get_triangle_scalene_all_mixedTypeTwo(self):
-#         result = get_triangle_type(4, 3.0, 2)
-#         self.assertEqual(result, 'scalene')
-#
-#     def test_get_triangle_scalene_all_mixedTypeThree(self):
-#         result = get_triangle_type(4, 3, 2.0)
-#         self.assertEqual(result, 'scalene')
-#
-#     "SCALENE with invalid lengths"
-#
-#     def test_get_triangle_scalene_invalidAreaOne(self):
-#         result = get_triangle_type(1, 2, 3)
-#         self.assertEqual(result, 'scalene')
-#
-#     def test_get_triangle_scalene_invalidAreaTwo(self):
-#         result = get_triangle_type(3, 1, 2)
-#         self.assertEqual(result, 'scalene')
-#
-#     def test_get_triangle_scalene_invalidAreaThree(self):
-#         result = get_triangle_type(1, 3, 2)
-#         self.assertEqual(result, 'scalene')
-#
-#     def test_get_triangle_scalene_invalidAreaFour(self):
-#         result = get_triangle_type(1, 2, 4)
-#         self.assertEqual(result, 'scalene')
-#
-#     def test_get_triangle_scalene_invalidAreaFive(self):
-#         result = get_triangle_type(4, 1, 2)
-#         self.assertEqual(result, 'scalene')
-#
-#     def test_get_triangle_scalene_invalidAreaSix(self):
-#         result = get_triangle_type(1, 4, 2)
-#         self.assertEqual(result, 'scalene')
-#
-#     "ISO where a=b"
-#
-#     def test_get_triangle_isosceles_all_int_ab(self):
-#         result = get_triangle_type(2, 2, 1)
-#         self.assertEqual(result, 'isosceles')
-#
-#     "ISO where a=c"
-#
-#     def test_get_triangle_isosceles_all_int_ac(self):
-#         result = get_triangle_type(2, 1, 2)
-#         self.assertEqual(result, 'isosceles')
-#
-#     "ISO where b=c"
-#
-#     def test_get_triangle_isosceles_all_int_bc(self):
-#         result = get_triangle_type(1, 2, 2)
-#         self.assertEqual(result, 'isosceles')
-#
-#     "ISO with mixed type a=b"
-#
-#     def test_get_triangle_isosceles_all_mixedType_ab(self):
-#         result = get_triangle_type(2.0, 2, 1)
-#         self.assertEqual(result, 'isosceles')
-#
-#     "a=c"
-#
-#     def test_get_triangle_isosceles_all_mixedType_ac(self):
-#         result = get_triangle_type(2, 1.0, 2)
-#         self.assertEqual(result, 'isosceles')
-#
-#     "b=c"
-#
-#     def test_get_triangle_isosceles_all_mixedType_bc(self):
-#         result = get_triangle_type(1.0, 2, 2)
-#         self.assertEqual(result, 'isosceles')
-#
-#     "EQUILATERAL with 1 side = 0"
-#
-#     def test_get_triangle_equilateral_oneSideZero(self):
-#         result = get_triangle_type(0, 1, 1)
-#         self.assertEqual(result, 'equilateral')
-#
-#     "SCALENE with 1 side = 0"
-#
-#     def test_get_triangle_scalene_oneSideZero(self):
-#         result = get_triangle_type(4, 0, 2)
-#         self.assertEqual(result, 'scalene')
-#
-#     "ISO with 1 side = 0"
-#
-#     def test_get_triangle_isosceles_oneSideZero(self):
-#         result = get_triangle_type(2, 2, 0)
-#         self.assertEqual(result, 'isosceles')
-#
-#     "EQUILATERAL with all sides = 0"
-#
-#     def test_get_triangle_equilateral_allSideZero(self):
-#         result = get_triangle_type(0, 0, 0)
-#         self.assertEqual(result, 'equilateral')
-#
-#     "SCALENE with all sides = 0"
-#
-#     def test_get_triangle_scalene_allSideZero(self):
-#         result = get_triangle_type(0, 0, 0)
-#         self.assertEqual(result, 'scalene')
-#
-#     "ISO with all sides = 0"
-#
-#     def test_get_triangle_isosceles_allSideZero(self):
-#         result = get_triangle_type(0, 0, 0)
-#         self.assertEqual(result, 'isosceles')
-#
-#     "EQUILATERAL with 1 side < 0"
-#
-#     def test_get_triangle_equilateral_oneSideNeg(self):
-#         result = get_triangle_type(-1, 1, 1)
-#         self.assertEqual(result, 'equilateral')
-#
-#     "SCALENE with 1 side < 0"
-#
-#     def test_get_triangle_scalene_oneSideNeg(self):
-#         result = get_triangle_type(4, -3, 2)
-#         self.assertEqual(result, 'scalene')
-#
-#     "ISO with 1 side < 0"
-#
-#     def test_get_triangle_isosceles_oneSideNeg(self):
-#         result = get_triangle_type(2, 1, -2)
-#         self.assertEqual(result, 'isosceles')
-#
-#     "Too many ARGS"
-#
-#     def test_get_triangle_tooManyParams(self):
-#         self.assertRaises(Exception, get_triangle_type, 1, 2, 3, 4)
+class TestGetTriangleType(TestCase):
+    "EQUILATERAL with all INT"
+
+    def test_get_triangle_equilateral_all_int(self):
+        result = get_triangle_type(1, 1, 1)
+        self.assertEqual(result, 'equilateral')
+
+    "EQUILATERAL with mixed type"
+
+    def test_get_triangle_equilateral_all_mixedTypeOne(self):
+        result = get_triangle_type(1.0, 1, 1)
+        self.assertEqual(result, 'equilateral')
+
+    def test_get_triangle_equilateral_all_mixedTypeTwo(self):
+        result = get_triangle_type(1, 1.0, 1)
+        self.assertEqual(result, 'equilateral')
+
+    def test_get_triangle_equilateral_all_mixedTypeThree(self):
+        result = get_triangle_type(1, 1, 1.0)
+        self.assertEqual(result, 'equilateral')
+
+    "SCALENE with all INT"
+
+    def test_get_triangle_scalene_all_int(self):
+        result = get_triangle_type(4, 3, 2)
+        self.assertEqual(result, 'scalene')
+
+    "SCALENE with mixed type"
+
+    def test_get_triangle_scalene_all_mixedTypeOne(self):
+        result = get_triangle_type(4.0, 3, 2)
+        self.assertEqual(result, 'scalene')
+
+    def test_get_triangle_scalene_all_mixedTypeTwo(self):
+        result = get_triangle_type(4, 3.0, 2)
+        self.assertEqual(result, 'scalene')
+
+    def test_get_triangle_scalene_all_mixedTypeThree(self):
+        result = get_triangle_type(4, 3, 2.0)
+        self.assertEqual(result, 'scalene')
+
+    "SCALENE with invalid lengths"
+
+    def test_get_triangle_scalene_invalidAreaOne(self):
+        result = get_triangle_type(1, 2, 3)
+        self.assertEqual(result, 'scalene')
+
+    def test_get_triangle_scalene_invalidAreaTwo(self):
+        result = get_triangle_type(3, 1, 2)
+        self.assertEqual(result, 'scalene')
+
+    def test_get_triangle_scalene_invalidAreaThree(self):
+        result = get_triangle_type(1, 3, 2)
+        self.assertEqual(result, 'scalene')
+
+    def test_get_triangle_scalene_invalidAreaFour(self):
+        result = get_triangle_type(1, 2, 4)
+        self.assertEqual(result, 'scalene')
+
+    def test_get_triangle_scalene_invalidAreaFive(self):
+        result = get_triangle_type(4, 1, 2)
+        self.assertEqual(result, 'scalene')
+
+    def test_get_triangle_scalene_invalidAreaSix(self):
+        result = get_triangle_type(1, 4, 2)
+        self.assertEqual(result, 'scalene')
+
+    "ISO where a=b"
+
+    def test_get_triangle_isosceles_all_int_ab(self):
+        result = get_triangle_type(2, 2, 1)
+        self.assertEqual(result, 'isosceles')
+
+    "ISO where a=c"
+
+    def test_get_triangle_isosceles_all_int_ac(self):
+        result = get_triangle_type(2, 1, 2)
+        self.assertEqual(result, 'isosceles')
+
+    "ISO where b=c"
+
+    def test_get_triangle_isosceles_all_int_bc(self):
+        result = get_triangle_type(1, 2, 2)
+        self.assertEqual(result, 'isosceles')
+
+    "ISO with mixed type a=b"
+
+    def test_get_triangle_isosceles_all_mixedType_ab(self):
+        result = get_triangle_type(2.0, 2, 1)
+        self.assertEqual(result, 'isosceles')
+
+    "a=c"
+
+    def test_get_triangle_isosceles_all_mixedType_ac(self):
+        result = get_triangle_type(2, 1.0, 2)
+        self.assertEqual(result, 'isosceles')
+
+    "b=c"
+
+    def test_get_triangle_isosceles_all_mixedType_bc(self):
+        result = get_triangle_type(1.0, 2, 2)
+        self.assertEqual(result, 'isosceles')
+
+    "EQUILATERAL with 1 side = 0"
+
+    def test_get_triangle_equilateral_oneSideZero(self):
+        result = get_triangle_type(0, 1, 1)
+        self.assertEqual(result, 'equilateral')
+
+    "SCALENE with 1 side = 0"
+
+    def test_get_triangle_scalene_oneSideZero(self):
+        result = get_triangle_type(4, 0, 2)
+        self.assertEqual(result, 'scalene')
+
+    "ISO with 1 side = 0"
+
+    def test_get_triangle_isosceles_oneSideZero(self):
+        result = get_triangle_type(2, 2, 0)
+        self.assertEqual(result, 'isosceles')
+
+    "EQUILATERAL with all sides = 0"
+
+    def test_get_triangle_equilateral_allSideZero(self):
+        result = get_triangle_type(0, 0, 0)
+        self.assertEqual(result, 'equilateral')
+
+    "SCALENE with all sides = 0"
+
+    def test_get_triangle_scalene_allSideZero(self):
+        result = get_triangle_type(0, 0, 0)
+        self.assertEqual(result, 'scalene')
+
+    "ISO with all sides = 0"
+
+    def test_get_triangle_isosceles_allSideZero(self):
+        result = get_triangle_type(0, 0, 0)
+        self.assertEqual(result, 'isosceles')
+
+    "EQUILATERAL with 1 side < 0"
+
+    def test_get_triangle_equilateral_oneSideNeg(self):
+        result = get_triangle_type(-1, 1, 1)
+        self.assertEqual(result, 'equilateral')
+
+    "SCALENE with 1 side < 0"
+
+    def test_get_triangle_scalene_oneSideNeg(self):
+        result = get_triangle_type(4, -3, 2)
+        self.assertEqual(result, 'scalene')
+
+    "ISO with 1 side < 0"
+
+    def test_get_triangle_isosceles_oneSideNeg(self):
+        result = get_triangle_type(2, 1, -2)
+        self.assertEqual(result, 'isosceles')
+
+    "Too many ARGS"
+
+    def test_get_triangle_tooManyParams(self):
+        self.assertRaises(Exception, get_triangle_type, 1, 2, 3, 4)
 
 "SQUARE TESTS__________________________________________________________________________________________________"
 
